ecddf41cf1d9839054ca59059af296af2d739572
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-01 22:21:06 +0100

    read document html with fs

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
-let dataForJson; //escribir aqui la data final
+let dataForJson = {}; //escribir aqui la data final
 const pathToWrite = __dirname + `/data.json`;
 
 let data = fs.readFileSync(`${__dirname}/webToCheck.html`, "utf8");
 console.log(data);
 
 
-
+//VERSION1
 // fs.writeFile(pathToWrite, dataForJson, (err) => {
 //     if (err) {
 //         console.log("Error", err);
@@ -14,3 +14,11 @@ console.log(data);
 //     }
 //     console.log("Write File Sucess");
 // });
+
+//VERSION2
+// try {
+//     const stringedObj = JSON.stringify(dataForJson, null, 8);
+//     fs.writeFileSync(pathToWrite, stringedObj);
+// } catch (e) {
+//     console.log("Error in making string: ", e); //logs 'ReferenceError: asdfasfasf is not defined'
+// }
