ea15a789b1e76f50a9f22de982e5f3cf623dd30a
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-02 12:13:30 +0100

    start fetching, not working yet

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const fs = require("fs");
 const cheerio = require("cheerio");
-const { children } = require("cheerio/lib/api/traversing");
+var request = require("request");
 
-// let dataForJson = {}; //escribir aqui la data final
-const pathToWrite = __dirname + `/data.json`;
+const pathToWrite = __dirname + `/properties.json`;
 
 let data = fs.readFileSync(`${__dirname}/webToCheck.html`, "utf8");
 
 const getDataFromHTML = (html) => {
-    let result = []
-    let arrayOfPrices = []
-    let arrOfNamesRaw =[]
-    let arrayOfNames =[]
+    let result = [];
+    let arrayOfPrices = [];
+    let arrOfNamesRaw = [];
+    let arrayOfNames = [];
     const $ = cheerio.load(html, null, false);
     $.html();
     const propertyNames = $("h5", ".property-content").each(function (i, e) {
@@ -19,33 +18,59 @@ const getDataFromHTML = (html) => {
     });
 
     const propertyPrices = $(".sc-fKFyDc").each(function (i, e) {
-        arrayOfPrices[i] = $(this).text()
+        arrayOfPrices[i] = $(this).text();
     });
-    
+
     const propertyLocations = $("span", ".sc-bBXqnf").text();
-    const cleanPropertyLocations = propertyLocations.replace(/\s+/g, ' ').trim()
+    const cleanPropertyLocations = propertyLocations
+        .replace(/\s+/g, " ")
+        .trim();
 
-    const arrayOfLocations = cleanPropertyLocations.split(",")
+    const arrayOfLocations = cleanPropertyLocations.split(",");
 
-    for(let i = 0; i< arrOfNamesRaw.length;i++){
-        arrayOfNames.push(arrOfNamesRaw[i].replace(/\s+/g, ' '))
+    for (let i = 0; i < arrOfNamesRaw.length; i++) {
+        arrayOfNames.push(arrOfNamesRaw[i].replace(/\s+/g, " "));
     }
 
-    for(j =0; j<arrOfNamesRaw.length;j++){
-          const obj ={
-              property: arrayOfNames[j],
-              location: arrayOfLocations[j],
-              price: arrayOfPrices[j]
-          }
-          result.push(obj)
+    for (j = 0; j < arrOfNamesRaw.length; j++) {
+        const obj = {
+            property: arrayOfNames[j],
+            location: arrayOfLocations[j],
+            price: arrayOfPrices[j],
+        };
+        result.push(obj);
     }
 
-    return result
+    return result;
 };
 
-console.log(getDataFromHTML(data));
+// let dataForJson = fetch(
+//     {
+//         method: "GET",
+//         url: "https://apartamentosrd.com.do/propiedades?city=60&currency=RD&listing_type=1&page=1",
+//     },
+//     (err, res, body) => {
+//         if (err) return console.error(err);
 
-let dataForJson = getDataFromHTML(data);
+//         getDataFromHTML(body);
+//     }
+// );
+
+let dataForJson = request(
+    {
+        uri: "https://apartamentosrd.com.do/propiedades?city=60&currency=RD&listing_type=1&page=1",
+    },
+    function (error, response, body) {
+        let dataJson = getDataFromHTML(JSON.parse(body));
+        return dataJson;
+    }
+);
+
+console.log(dataForJson)
+
+
+// let dataForJson = getDataFromHTML(data);
+
 try {
     const stringedObj = JSON.stringify(dataForJson, null, 8);
     fs.writeFileSync(pathToWrite, stringedObj);
