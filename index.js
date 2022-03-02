c1c574d98ffaef9dad60d8abff446950694a55c4
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-03-02 11:43:16 +0100

    sabe object in json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const cheerio = require("cheerio");
 const { children } = require("cheerio/lib/api/traversing");
 
-let dataForJson = {}; //escribir aqui la data final
+// let dataForJson = {}; //escribir aqui la data final
 const pathToWrite = __dirname + `/data.json`;
 
 let data = fs.readFileSync(`${__dirname}/webToCheck.html`, "utf8");
@@ -45,19 +45,10 @@ const getDataFromHTML = (html) => {
 
 console.log(getDataFromHTML(data));
 
-//VERSION1
-// fs.writeFile(pathToWrite, dataForJson, (err) => {
-//     if (err) {
-//         console.log("Error", err);
-//         return;
-//     }
-//     console.log("Write File Sucess");
-// });
-
-//VERSION2
-// try {
-//     const stringedObj = JSON.stringify(dataForJson, null, 8);
-//     fs.writeFileSync(pathToWrite, stringedObj);
-// } catch (e) {
-//     console.log("Error in making string: ", e); //logs 'ReferenceError: asdfasfasf is not defined'
-// }
+let dataForJson = getDataFromHTML(data);
+try {
+    const stringedObj = JSON.stringify(dataForJson, null, 8);
+    fs.writeFileSync(pathToWrite, stringedObj);
+} catch (e) {
+    console.log("Error making string: ", e);
+}
